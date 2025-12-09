90e344139ed3a111597135d2a047ef7c8d65e0f5
Ben Keen <ben.keen@gmail.com>
2025-12-08 21:52:48 -0800

    adding webpack build

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const mode = argv.mode === 'production' ? 'production' : 'development';
+
+  var config = {
+    mode,
+
+    entry: {
+      app: path.resolve(__dirname, 'src/index.tsx'),
+    },
+
+    output: {
+      path: path.join(__dirname, 'dist'),
+      publicPath: '/',
+      chunkFilename:
+        mode === 'development' ? '[name].js' : '[name]-[fullhash].js',
+      filename: mode === 'development' ? '[name].js' : '[name]-[fullhash].js',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: [{ loader: 'ts-loader' }],
+        },
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
+            },
+          ],
+          exclude: '/node_modules',
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            {
+              loader: '@teamsupercell/typings-for-css-modules-loader',
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[name]__[local]--[fullhash:base64:3]',
+                },
+                url: false,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {},
+            },
+          ],
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src/index.html'),
+      }),
+    ],
+
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+  };
+
+  if (mode === 'development') {
+    config.devServer = {
+      historyApiFallback: true,
+      static: path.join(__dirname, 'dist'),
+      // publicPath: 'http://localhost:9000',
+      port: process.env.GD_WEB_SERVER_PORT,
+      open: true,
+    };
+  }
+
+  if (mode === 'production') {
+    config.optimization.minimizer = [new TerserPlugin()];
+    config.optimization.minimize = true;
+  }
+
+  return config;
+};
